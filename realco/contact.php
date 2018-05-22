f4d58579ce41d79215835352e1aaf9554f87d80e
Gavin <gavin@texascreative.com>
2018-05-22 15:23:58 -0500

    continued work on Webpage class

--- realco/contact.php
+++ realco/contact.php
@@ -9,15 +9,19 @@ $honeypotInputs = $oForm->inputs();
 ?>
 <html>
 <head>
-  <title>Contact – Real Estate Co</title>
+  <title>Contact – Real Estate Co.</title>
   
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   
   <link rel="stylesheet" type="text/css" href="style.css">
   <script src='https://www.google.com/recaptcha/api.js'></script>
+  <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" 
+  integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" 
+  crossorigin="anonymous"></script>
 </head>
 
 <body>
+<!-- <i class="fas fa-home fa-10x"></i> -->
   <div id="page-wrap" class="container">
     <div id="contact">
       <h3>Real Estate Co.</h3>
--- src/Webpage.php
+++ src/Webpage.php
@@ -5,16 +5,38 @@ class Webpage {
 
   private $myUrl;
 
-  public function __construct(string $url)
-  {
+  public function __construct(string $url) {
     $this->myUrl = $url;
   }
 
-  public function showHighlight(string $requestedUrl = null) {
+  public function showHighlight(string $requestedUrl) {
     $requestedUrl = $this->myUrl;
+
     return $requestedUrl;
   }
 
+  private function getFilename(string $url) {
+    $filename = '';
+    $uriSegments = explode('/', $url);
+
+    foreach($uriSegments as $segment) {
+      if (preg_match('(?!\/)\w+(?<!\/)', $segment)) {
+        $filename = $segment;
+      } else {
+        continue;
+      }
+    }
+
+    return $segment;
+  }
+
+  private function checkSection(string $url) {
+    
+  }
+
+  private function isLanding() {
+
+  }
 }
 
 ?>
