c4fc88bd0b1af8111cf791ee6d9aa23e54d6cb62
gavinv <gavinvaught@gmail.com>
2018-05-24 15:43:15 -0500

    fixed favicon

--- public_html/templates/head.php
+++ public_html/templates/head.php
@@ -3,15 +3,16 @@ ini_set("include_path", "../".ini_get("include_path"));
 ?>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <title><?php echo (isset($pageName)) ? $pagename.' – ' : '' ;?>Gavin Vaught</title>
+  <title><?php echo (isset($pageName)) ? $pageName.' – ' : '' ;?>Gavin Vaught</title>
   <!-- Favicon -->
-  <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png">
-  <link rel="manifest" href="images/favicon/site.webmanifest">
-  <link rel="mask-icon" href="images/favicon/safari-pinned-tab.svg" color="#424242">
+  <link rel="shortcut icon" href="/favicon.ico?">
+  <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png?">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png?">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png?">
+  <link rel="manifest" href="/favicon/site.webmanifest?">
+  <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg?" color="#424242">
   <meta name="msapplication-TileColor" content="#bababa">
-  <meta name="msapplication-TileImage" content="images/favicon/mstile-144x144.png">
+  <meta name="msapplication-TileImage" content="/favicon/mstile-144x144.png?">
   <meta name="theme-color" content="#2e2e2e">
   
   <link rel="stylesheet" type="text/css" href="css/styles.css">
