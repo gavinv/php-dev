2cfdcc6370c1aa3507a2423844b0d38d07fc0f6c
gavinv <gavinvaught@gmail.com>
2018-05-23 23:48:09 -0500

    style changes. updated homepage

--- index.php
+++ index.php
@@ -1,13 +1,39 @@
 <?php
-
+require 'vendor/autoload.php';
 ?>
 <html>
-  <head>
-    <title>Gavin Vaught</title>
-  </head>
-  <body>
-    Under maintenance..
-    <br><br>
-    Please check back in a few~
-  </body>
+<head>
+  <title>Gavin Vaught</title>
+  <link rel="stylesheet" type="text/css" href="../css/styles.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
+<body>
+  <div id="main" class="container">
+    <div class="content">
+      <h1>Gavin Vaught</h1>
+      <hr>
+      <h2>Currently under development...</h2>
+      <h4>Please check back soon~</h4>
+      <p>
+        In the meantime, feel free to check out my 
+        <a target="_blank" href="https://github.com/gavinv">GitHub</a>
+        and
+        <a target="_blank" href="https://linkedin.com/in/gavinvaught">LinkedIn</a>.
+      </p>
+      <p>
+        If you have any immediate questions or concerns, you may also email me 
+        <a href="mailto:gavinvaught@gmail.com">here</a>.
+      </p>
+      <hr>
+      <div class="bottom-extras content-center">
+        <b>Some recent tests:</b>
+        <p>
+          <a href="/tests/uri-checker.php">URI Checker</a> |
+          <a href="/tests/breadcrumbs.php">Breadcrumbs</a> |
+          <a href="/tests/realco/contact.php">Contact Form</a>
+        </p>
+      </div>
+    </div>
+  </div>
+</body>
 </html>
