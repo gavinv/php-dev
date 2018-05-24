47d6f3eefce7b40cbaaf27a9f946ac19736e9a1d
gavinv <gavinvaught@gmail.com>
2018-05-24 15:27:49 -0500

    fixed directories in headers

--- public_html/tests/realco/thank-you.php
+++ public_html/tests/realco/thank-you.php
@@ -2,16 +2,16 @@
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <title><?php echo (isset($pageName)) ? $pagename.' – ' : '' ;?>Gavin Vaught</title>
   <!-- Favicon -->
-  <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png">
-  <link rel="manifest" href="images/favicon/site.webmanifest">
-  <link rel="mask-icon" href="images/favicon/safari-pinned-tab.svg" color="#424242">
+  <link rel="apple-touch-icon" sizes="180x180" href="../../images/favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="../../images/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="../../images/favicon/favicon-16x16.png">
+  <link rel="manifest" href="../../images/favicon/site.webmanifest">
+  <link rel="mask-icon" href="../../images/favicon/safari-pinned-tab.svg" color="#424242">
   <meta name="msapplication-TileColor" content="#bababa">
   <meta name="msapplication-TileImage" content="images/favicon/mstile-144x144.png">
   <meta name="theme-color" content="#2e2e2e">
   
-  <link rel="stylesheet" type="text/css" href="css/styles.css">
+  <link rel="stylesheet" type="text/css" href="../../css/styles.css">
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src='https://www.google.com/recaptcha/api.js'></script>
