efbe890831332ba499c4585e48bf14c90dbb1902
gavinv <gavinvaught@gmail.com>
2018-05-25 12:14:39 +0000

    pushing to prod

--- config/pdo_connect.php
+++ config/pdo_connect.php
@@ -1,8 +1,12 @@
 <?php
-require_once __DIR__ . '/dev.conf.php';
+require_once __DIR__ . '/live.conf.php';
 
-$dbc = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
+try {
+  $dbc = new PDO('mysql:host='.DB_HOST.';port=443;dbname='.DB_NAME, DB_USER, DB_PASS);
 
-$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
 
 ?>
---
+++ html/templates/head.php
@@ -0,0 +1,25 @@
+<?php
+ini_set("include_path", "../".ini_get("include_path"));
+?>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <title><?php echo (isset($pageName)) ? $pageName.' – ' : '' ;?>Gavin Vaught</title>
+  <!-- Favicon -->
+  <link rel="shortcut icon" href="/favicon.ico">
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  <link rel="manifest" href="/site.webmanifest">
+  <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#424242">
+  <meta name="msapplication-TileColor" content="#bababa">
+  <meta name="msapplication-TileImage" content="/mstile-144x144.png">
+  <meta name="theme-color" content="#2e2e2e">
+  
+  <link rel="stylesheet" type="text/css" href="css/styles.css">
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src='https://www.google.com/recaptcha/api.js'></script>
+  <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" 
+  integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" 
+  crossorigin="anonymous"></script>
+</head>
